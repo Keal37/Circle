841820bf110e9b3a6ab72eb8b39b08c9a3c1358f
Keal37 <110300358+Keal37@users.noreply.github.com>
2026-04-15 16:02:06 +0300

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,26 @@
+const socket = io();
+
+let currentCircle = "";
+
+function joinCircle() {
+  currentCircle = document.getElementById("circleInput").value;
+  socket.emit("joinCircle", currentCircle);
+}
+
+function send() {
+  const text = document.getElementById("msg").value;
+
+  socket.emit("message", {
+    circle: currentCircle,
+    text: text
+  });
+
+  document.getElementById("msg").value = "";
+}
+
+socket.on("message", (data) => {
+  const div = document.createElement("div");
+  div.innerText = `[${data.circle}] ${data.text}`;
+  document.getElementById("messages").appendChild(div);
+});
+
