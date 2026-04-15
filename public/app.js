9ebf401b6a20ff86e5b36cadad2f02ca9846cadb
Keal37 <110300358+Keal37@users.noreply.github.com>
2026-04-15 19:34:26 +0300

    Refactor message handling to include username

--- public/app.js
+++ public/app.js
@@ -1,26 +1,59 @@
 const socket = io("https://circle-backend-s7dz.onrender.com");
 
-let currentCircle = "";
+// --------------------
+// USERNAME SETUP
+// --------------------
+let username = localStorage.getItem("username");
 
-function joinCircle() {
-  currentCircle = document.getElementById("circleInput").value;
-  socket.emit("joinCircle", currentCircle);
+if (!username) {
+  username = prompt("Enter your username:");
+  localStorage.setItem("username", username);
 }
 
+// --------------------
+// CURRENT CIRCLE
+// --------------------
+let currentCircle = "";
+
+// --------------------
+// JOIN CIRCLE
+// --------------------
+function joinCircle() {
+  const input = document.getElementById("circleInput");
+  currentCircle = input.value;
+
+  socket.emit("joinCircle", currentCircle);
+
+  input.value = "";
+}
+
+// --------------------
+// SEND MESSAGE
+// --------------------
 function send() {
   const text = document.getElementById("msg").value;
 
+  if (!currentCircle) {
+    alert("Join a Circle first!");
+    return;
+  }
+
   socket.emit("message", {
     circle: currentCircle,
-    text: text
+    text: text,
+    username: username
   });
 
   document.getElementById("msg").value = "";
 }
 
+// --------------------
+// RECEIVE MESSAGE
+// --------------------
 socket.on("message", (data) => {
   const div = document.createElement("div");
-  div.innerText = `[${data.circle}] ${data.text}`;
+
+  div.innerText = `${data.username}: ${data.text}`;
+
   document.getElementById("messages").appendChild(div);
 });
-
